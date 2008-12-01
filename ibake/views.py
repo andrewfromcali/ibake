cf81510bae85202b864e9f4bb138971d8c1a7fe3
aa <andrew@geni.com>
2008-11-30 19:08:59 -0800

    form

--- ibake/views.py
+++ ibake/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
-import django;
+from django import forms
 
+class ContactForm(forms.Form):
+    subject = forms.CharField(max_length=100)
+    message = forms.CharField()
+    sender = forms.EmailField()
+    cc_myself = forms.BooleanField(required=False)
+    
 def main(request):
-  return render_to_response('main/main.html', {
-      'form': django.VERSION
-  })
+    if request.method == 'POST': # If the form has been submitted...
+        form = ContactForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            # Process the data in form.cleaned_data
+            # ...
+            return HttpResponseRedirect('/thanks/') # Redirect after POST
+    else:
+        form = ContactForm() # An unbound form
+
+    return render_to_response('main/main.html', {
+        'form': form,
+    })
