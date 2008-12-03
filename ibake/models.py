9715d71cedaf8170cbb506cf5204e17510fcdbb5
aa <andrew@geni.com>
2008-12-02 18:40:56 -0800

    new items

--- ibake/models.py
+++ ibake/models.py
@@ -1,8 +1,19 @@
 from google.appengine.ext import db
+import re
 
+class Callable:
+    def __init__(self, anycallable):
+        self.__call__ = anycallable
+        
 class Item(db.Model):
   parent_permalink = db.StringProperty(required=True)
   permalink        = db.StringProperty(required=True)
   name             = db.StringProperty(required=True)
   description      = db.TextProperty
   link             = db.StringProperty
+  
+  def get_friendly_url(title):
+    return re.sub('-+', '-', 
+             re.sub('[^\w-]', '', 
+               re.sub('\s+', '-', title.strip().lower())))
+  get_friendly_url = Callable(get_friendly_url)
--- ibake/views.py
+++ ibake/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
 from django import forms
 from ibake.models import Item
+from google.appengine.ext import db
 
 class ItemField(forms.Field):
     def clean(self, value):
@@ -13,6 +14,10 @@ class ItemField(forms.Field):
         if len(value) > 50:
             raise forms.ValidationError('This field is over 50 characters.')
 
+        query = db.Query(Item)
+        query.filter('permalink =', Item.get_friendly_url(value))
+        if query.get():
+            raise forms.ValidationError('This item already exists.')
         return value
         
 class ContactForm(forms.Form):
@@ -22,12 +27,17 @@ def main(request):
     if request.method == 'POST': 
         form = ContactForm(request.POST) 
         if form.is_valid():
-            item = form.cleaned_data['item']
-            
+            name = form.cleaned_data['item']
+            sp = Item(name=name,permalink=Item.get_friendly_url(name),parent_permalink='-')
+            sp.put()
             return HttpResponseRedirect('/') 
     else:
         form = ContactForm()
 
+    query = Item.all()
+    query.order('permalink')
+
     return render_to_response('main/main.html', {
         'form': form,
+        'items': query.fetch(1000, 0),
     })
